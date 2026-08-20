e356a8ceac022ce6360d4701435df850d648b015
ssikokr <karabosiko1.0@gmail.com>
2026-08-20 12:21:48 +0200

    Add multiply function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function add(a, b) { return a + b; }
 function subtract(a, b) { return a - b; }
 function multiply(a, b) { return a * b; }
+function multiply(a, b) { return a * b; }
 module.exports = { add, subtract };
